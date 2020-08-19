c85787c33354a34f152cfae9161add37c50914ca
Raul <raul.saez@apposing.co.uk>
2020-08-19 10:58:13 +0100

    Replace illustration with svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import GlobalStyle from "../components/globalStyles"
 import { ctaColor, btnTxtColor } from "../components/variables"
-// @ts-ignore
-import illustration from "../images/illustration.svg"
 import { IntlProvider, FormattedMessage } from "react-intl"
 
 const StyledContent = styled.div`
@@ -17,12 +15,14 @@ const StyledContent = styled.div`
   @media (min-width: 1000px) {
     grid-template-columns: 1fr 1fr;
   }
-  img {
+  img,
+  svg {
+    fill: #ff8906;
     display: none;
     @media (min-width: 1000px) {
       display: block;
     }
-    max-height: 450px;
+    max-height: 350px;
     width: 100%;
     align-self: center;
   }
@@ -92,7 +92,17 @@ const IndexPage: React.FC = () => {
               <FormattedMessage id="contact" />
             </StyledLink>
           </div>
-          <img src={illustration} alt="Illustration" />
+          <svg fill="currentColor" viewBox="0 0 16 16">
+            <path
+              fillRule="evenodd"
+              d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z"
+            />
+            <path
+              fill="#a7a9be"
+              fillRule="evenodd"
+              d="M6.854 4.646a.5.5 0 010 .708L4.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0zm2.292 0a.5.5 0 000 .708L11.793 8l-2.647 2.646a.5.5 0 00.708.708l3-3a.5.5 0 000-.708l-3-3a.5.5 0 00-.708 0z"
+            />
+          </svg>
         </StyledContent>
       </Layout>
     </IntlProvider>
