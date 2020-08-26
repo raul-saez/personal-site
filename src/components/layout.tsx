372b47d5c41fab613b11007ba3ad39b57253e84a
Raul <raul.saez@apposing.co.uk>
2020-08-26 12:29:58 +0100

    Add icons SVGs

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,16 +2,34 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import styled from "styled-components"
-import { pColor } from "./variables"
+import { pColor, btnTxtColor, ctaColor } from "./variables"
 // @ts-ignore
 import favicon from "../images/favicon.png"
 import Helmet from "react-helmet"
 import { FormattedMessage } from "react-intl"
+import GoodreadsIcon from "./goodreads"
+import GithubIcon from "./github"
+import LinkedinIcon from "./linkedin"
 
 const StyledFooter = styled.footer`
   text-align: center;
   color: ${pColor};
   padding: 3rem 3rem 2rem 3rem;
+`
+
+const StyledLinks = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, max-content);
+  gap: 10px;
+  justify-content: center;
+  svg {
+    width: 40px;
+    height: auto;
+    fill: ${btnTxtColor};
+  }
+  svg:hover {
+    fill: ${ctaColor};
+  }
 `
 
 interface Props {
@@ -50,6 +68,17 @@ const Layout: React.FC<Props> = ({ children, lg }) => {
       <div>
         <main>{children}</main>
         <StyledFooter>
+          <StyledLinks>
+            <a href="https://www.github.com/raul-saez">
+              <GithubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/raul-saez/">
+              <LinkedinIcon />
+            </a>
+            <a href="https://www.goodreads.com/user/show/117148033-raul">
+              <GoodreadsIcon />
+            </a>
+          </StyledLinks>
           ©️ <FormattedMessage id="footer" /> ♥️
         </StyledFooter>
       </div>
